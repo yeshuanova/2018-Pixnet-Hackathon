1c369c2d02d4684e59018c73529a48ead2be9e9f
Peter.Li <yeshuanova@gmail.com>
2018-08-06 18:32:20 +0000

    Add prediction code using generative inpainting model.

--- code/generative_inpainting/test.py
+++ code/generative_inpainting/test.py
@@ -20,7 +20,7 @@ parser.add_argument('--checkpoint_dir', default='', type=str,
 
 
 if __name__ == "__main__":
-    ng.get_gpus(nums_gpus=1, dedicated=False)
+    ng.get_gpus(num_gpus=1, dedicated=False)
     args = parser.parse_args()
 
     model = InpaintCAModel()
